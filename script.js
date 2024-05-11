2900226cd96789514ff46c9a8bda938d93eb727e
Людмила Ивченко <95207405+Lysiii2012@users.noreply.github.com>
2024-05-11 21:38:28 +0300

    prompt

---
+++ script.js
@@ -0,0 +1,7 @@
+let name = prompt('Your name?');
+let age = prompt('hHow old are you?')
+let hobby = prompt("Your hobbie?")
+
+console.log(`${name} ${age} ${hobby}`)
+
+alert(`${name} ${age} ${hobby}`)
