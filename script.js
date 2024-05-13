0be7cfea6715edc3570f589ac5487445b6e8789d
Людмила Ивченко <95207405+Lysiii2012@users.noreply.github.com>
2024-05-13 21:10:58 +0300

    case les4

--- script.js
+++ script.js
@@ -1,22 +1,16 @@
-let numberUser = prompt('Введіть трьохзначне число:');
-let answ1 = +numberUser[0];
-let answ2 = +numberUser[1];
-let answ3 = +numberUser[2];
+let numOrStr = prompt('input number or string');
+console.log(numOrStr);
 
-//Замість + можно використовувати Number()
-
-let allEqual = answ1 === answ2 && answ2 === answ3;
-let anyEqual = answ1 === answ2 || answ2 === answ3 || answ1 === answ3;
-
-console.log("Чи всі цифри однакові:", allEqual ? "Так" : "Ні");
-console.log("Чи є серед цифр цифри однакові:", anyEqual ? "Так" : "Ні");
-
-if (allEqual) {
-    console.log("Всі цифри однакові:", answ1);
-} else if (anyEqual) {
-    console.log("Серед цифр є однакові:", answ1 === answ2 ? answ1 : (answ1 === answ3 ? answ1 : answ2));
-} else {
-    console.log("Цифри не повторюються.");
+switch(true) {
+  case numOrStr === null:
+    console.log('ви скасували');
+    break;
+  case numOrStr.trim() === '':
+    console.log('Empty String');
+    break;
+  case isNaN(+numOrStr):
+    console.log(' number is Ba_NaN');
+    break;
+  default:
+    console.log('OK!');
 }
-
-
