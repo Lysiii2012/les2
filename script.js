9d24d1e02f985a73472984774e45a4e83561448e
Людмила Ивченко <95207405+Lysiii2012@users.noreply.github.com>
2024-05-13 22:08:27 +0300

    dz

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
-let numOrStr = prompt('input number or string');
-console.log(numOrStr);
+let dateBirthday = 2024 - Number(prompt('Рік народження?'))
+let city = prompt('В якому місті ви проживаєте?')
+let hobby = prompt('Улюблений вид спорту?')
 
-switch(true) {
-  case numOrStr === null:
-    console.log('ви скасували');
-    break;
-  case numOrStr.trim() === '':
-    console.log('Empty String');
-    break;
-  case isNaN(+numOrStr):
-    console.log(' number is Ba_NaN');
-    break;
-  default:
-    console.log('OK!');
+
+if (dateBirthday === null || city === null || hobby === null) {
+    let message = "Шкода, що Ви не захотіли ввести свій(ю) ";
+    if (dateBirthday === null) {
+        message += "дату народження";
+    }
+    if (city === null) {
+        if (dateBirthday === null) {
+            message += ", місто";
+        } else {
+            message += "місто";
+        }
+    }
+    if (hobby === null) {
+        if (dateBirthday === null || city === null) {
+            message += " та";
+        }
+        message += " вид спорту";
+    }
+    console.log(message + ".");
 }
+
+switch (city) {
+    case 'Київ':
+        cityPerson = 'Ти живеш у столиці України';
+        break;
+    case 'Вашингтон':
+        cityPerson = 'Ти живеш у столиці США';
+        break;
+    case 'Лондон':
+        cityPerson = 'Ти живеш у столиці Англії';
+        break;
+    default:
+        cityPerson = `Ти живеш у місті ${city}`;
+        break;
+}
+
+confirm(`${dateBirthday}, ${cityPerson}`)
+
