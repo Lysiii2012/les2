557fe68621902f1649be328a8884ddef093e2b30
Людмила Ивченко <95207405+Lysiii2012@users.noreply.github.com>
2024-05-14 09:09:52 +0300

    doppractic

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let dateBirthday = Number(prompt('Рік народження?'));
 let city = prompt('В якому місті ви проживаєте?');
 let hobby = prompt('Улюблений вид спорту?');
 
-let dateBirthdayAnsv = currentYear - currentYear
+let dateBirthdayAnsv = currentYear - dateBirthday;
 
 switch (city) {
     case 'Київ':
@@ -15,19 +15,38 @@ switch (city) {
     case 'Лондон':
         cityPerson = 'Ти живеш у столиці Англії';
         break;
-    case null :
-        cityPerson = 'А не скажу я місто'
+    case null:
+        cityPerson = 'Не скажу';
         break;
     default:
         cityPerson = `Ти живеш у місті ${city}`;
         break;
 }
 
+
+switch (hobby) {
+    case 'футбол':
+        hobby = 'Круто! Хочеш стати Роналдо?';
+        break;
+    case 'бокс':
+        hobby = 'Круто! Хочеш стати Олександром Усиком?';
+        break;
+    case 'балет':
+        hobby = 'Круто! Хочеш стати як Єлизавета Биденко';
+        break;
+    case null:
+        hobby = 'Мене не цікавить спорт';
+        break;
+    default:
+        break;
+}
+
 if (dateBirthday === null || city === null || hobby === null) {
     let message = "Шкода, що Ви не захотіли ввести свій(ю) ";
-    if (dateBirthday === undefined || isNaN(dateBirthday)) {
+    if (dateBirthday === null || isNaN(dateBirthday) || dateBirthday === 0) {
         message += "дату народження";
     }
+
     if (city === null) {
         message += " місто";
     }
@@ -37,4 +56,5 @@ if (dateBirthday === null || city === null || hobby === null) {
     alert(`${message} (`);
 }
 
-confirm(`${dateBirthdayAnsv} років, ${cityPerson}`);
+confirm(`${dateBirthdayAnsv} , ${cityPerson}`);
+alert(hobby)
